ad7cf93d6deac6d240c66daaf1fc9fbe9e3a88b2
bhawiyuga <adhitya.bhawiyuga@gmail.com>
2018-03-19 09:47:46 +0700

    aaa

--- kelastka/06-skeleton_kuis/client_rumahsakit.py
+++ kelastka/06-skeleton_kuis/client_rumahsakit.py
@@ -1,29 +1,25 @@
 
 try : 
 	while(True) :
-		print ""
-		print "Sistem Informasi Pasien"
-		print "1. Masukkan data pasien"
-		print "2. Lihat data pasien"
-		print "3. Keluar"
+		print("Sistem Informasi Pasien")
+		print("1. Masukkan data pasien")
+		print("2. Lihat data pasien")
+		print("3. Keluar")
 		operasi = input("Pilih operasi : ")
 		if operasi == "1" :
-			print ""
-			print "Masukkan data pasien"
-			nik = raw_input("NIK : ")
-			nama = raw_input("Nama : ")
-			alamat = raw_input("Alamat : ")
-			penyakit = raw_input("Penyakit : ")
+			print("Masukkan data pasien")
+			nik = input("NIK : ")
+			nama = input("Nama : ")
+			alamat = input("Alamat : ")
+			penyakit = input("Penyakit : ")
 			poli = input("Pilihan poli : 1) Umum, 2) Jantung, 3) Paru : ")
 
 		elif operasi == "2" :
-			print ""
 			nik = input("NIK Pasien : ") 
-			print "Data Pasien dengan NIK "+ nik
+			print("Data Pasien dengan NIK "+ nik)
 			
 		else :
-			print ""
-			print "Keluar dari program"
+			print("Keluar dari program")
 			break
 except KeyboardInterrupt :
 	print("Keluar dari program")
