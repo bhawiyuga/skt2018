01502ac30c3de20c6cabdf4e67585a0de60f70c4
bhawiyuga <adhitya.bhawiyuga@gmail.com>
2018-03-20 14:16:58 +0700

    bb

--- kelasb/06-skeleton_kuis/server_rumahsakit.py
+++ kelasb/06-skeleton_kuis/server_rumahsakit.py
@@ -1,12 +1,31 @@
 import socket
 import threading
+from flask import Flask, request
+import paho.mqtt.client as mqtt_client
+
+app = Flask("Web App Mahasiswa")
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello"
+
+def handle_message(client, object, msg):
+    print("Topik : "+msg.topic+" Message : "+msg.payload.decode('ascii'))
 
 def handle_server():
-    print("Bagian Server")
-    # To Do : Bagian RPC / HTTP Server
+    print("Bagian Subscriber")
+    # To Do : Bagian subscriber
+    sub = mqtt_client.Client()
+    sub.connect("127.0.0.1", 1883)
+    sub.on_message = handle_message
+    sub.subscribe("/sensor/1")
+    sub.loop_forever()
 
-while True :    
-    t = threading.Thread(target=handle_thread, args=(conn,))
-    t.start()
 
-    # To Do : Bagian Subscriber
+t = threading.Thread(target=handle_server)
+t.start()
+
+print("Server HTTP run di port 7777")
+app.run(port=7777)
+
+# To Do : Bagian Subscriber
